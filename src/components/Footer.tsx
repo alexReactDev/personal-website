0b20d8ba5a68a76dbc180efb18658c74428da1e0
Alexander <alexander.work.mailbox@gmail.com>
2023-07-20 19:38:42 +0600

    Added contactsList to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,19 @@
+import ContactsList from "./ContactsList";
+import Logo from "./Logo";
+
 function Footer() {
 	return (
 		<footer className="footer bg-gray-800 text-white">
 			<div className="container">
-				<div className="p-3">
+				<div className="py-5">
+					<div className="flex mb-5 justify-between">
+						<div className="w-5/6">
+							<ContactsList />
+						</div>
+						<div className="w-1/6 flex justify-center">
+							<Logo min />
+						</div>
+					</div>
 					<p className="text-center">
 						© 2023 alexreactdev
 					</p>
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
-function Logo() {
+function Logo({ min = false }) {
 	return (
-		<div className="max-h-20">
-			<Image src="/images/logo/logo.png" width={227} height={80} alt="logo" className="max-h-full" />
+		<div>
+			<Link href="/">
+				{
+					min
+					?
+					<Image src="/images/logo/logo-icon.png" width={90} height={90} alt="logo icon" className="max-h-[100px]" />
+					:
+					<Image src="/images/logo/logo.png" width={227} height={80} alt="logo" className="max-h-20" />
+				}
+			</Link>
 		</div>
 	)
 }
