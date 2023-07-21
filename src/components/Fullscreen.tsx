2751078e947fbe4ffcfae32508cfec555d1f7257
Alexander <alexander.work.mailbox@gmail.com>
2023-07-21 14:34:06 +0600

    Corrected fullscreen adaptive

--- src/components/Fullscreen.tsx
+++ src/components/Fullscreen.tsx
@@ -5,10 +5,10 @@ function Fullscreen() {
 		<section className="flex fullscreen -mt-[128px] pt-[128px] bg-[url('/images/homepage/main.jpg')] bg-center bg-cover bg-no-repeat text-white">
 			<div className="flex flex-col container">
 				<div className="grow flex flex-col justify-center items-center text-center font-medium">
-					<h1 className="text-8xl mb-8">
+					<h1 className="text-4xl xm:text-6xl sm:text-8xl mb-8">
 						Hi. My name is Alexander.
 					</h1>
-					<h2 className="mb-10 text-4xl">
+					<h2 className="mb-10 text-2xl xm:text-3xl sm:text-4xl">
 						{"I'm frontend developer"}
 					</h2>
 					<ScrollDown />
