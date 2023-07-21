893245fbb5359d56b5bd6ffa201e22c9924973ba
Alexander <alexander.work.mailbox@gmail.com>
2023-07-21 15:38:55 +0600

    Added backend and principles blocks

--- src/components/Backend.tsx
+++ src/components/Backend.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+
 function Backend() {
 	return (
 		<section className="fullscreen">
-			backend
+			<div className="container">
+				<div className="flex flex-col-reverse items-center justify-center gap-3 xm:gap-8 lg:gap-0 lg:flex-row min-h-screen">
+					<div className="xm:w-3/4 lg:w-1/2 flex flex-col justify-center box-border lg:pr-24 text-center lg:text-left">
+						<h2 className="title font-accent">
+							I know what happens behind the scenes
+						</h2>
+						<p>
+							Non et, hic aperiam natus obcaecati, blanditiis maiores voluptates officia minus quibusdam dolore praesentium nam laboriosam earum provident reiciendis dicta explicabo vel itaque ut repellat iure, at, eos repudiandae! Autem. Molestiae sed, ea quam veniam. Totam deleniti voluptatibus quae veritatis officiis blanditiis, temporibus in vel! Minus nesciunt qui optio aut quis alias non beatae. Molestias voluptate expedita consequuntur nisi iusto. Tempora sed deserunt consequuntur, unde.
+						</p>
+					</div>
+					<div className="xm:w-3/4 lg:w-1/2 flex justify-center items-center">
+						<Image src="/images/homepage/backend/hero.png" width={600} height={600} alt="backend technologies" />
+					</div>
+				</div>
+			</div>
 		</section>
 	)
 }
--- src/components/Principles.tsx
+++ src/components/Principles.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
+
 function Principles() {
 	return (
 		<section className="fullscreen">
-			Principles
-		</section>
+		<div className="container">
+			<div className="flex flex-col-reverse items-center justify-center gap-3 xm:gap-8 lg:gap-0 lg:flex-row-reverse min-h-screen">
+				<div className="xm:w-3/4 lg:w-1/2 flex flex-col justify-center box-border lg:pl-24 text-center lg:text-right">
+					<h2 className="title font-accent">
+						Development in accordance with best practices
+					</h2>
+					<p>
+						Non et, hic aperiam natus obcaecati, blanditiis maiores voluptates officia minus quibusdam dolore praesentium nam laboriosam earum provident reiciendis dicta explicabo vel itaque ut repellat iure, at, eos repudiandae! Autem. Molestiae sed, ea quam veniam. Totam deleniti voluptatibus quae veritatis officiis blanditiis, temporibus in vel! Minus nesciunt qui optio aut quis alias non beatae. Molestias voluptate expedita consequuntur nisi iusto. Tempora sed deserunt consequuntur, unde.
+					</p>
+				</div>
+				<div className="xm:w-3/4 lg:w-1/2 flex justify-center items-center">
+					<Image src="/images/homepage/principles/hero.png" width={500} height={500} alt="programming principles" />
+				</div>
+			</div>
+		</div>
+	</section>
 	)
 }
 
