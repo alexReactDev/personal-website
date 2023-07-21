782a226613a046a57e7a5a587985ce872833e122
Alexander <alexander.work.mailbox@gmail.com>
2023-07-21 17:19:08 +0600

    Corrected BtnLink styles

--- src/components/BtnLink.tsx
+++ src/components/BtnLink.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 function BtnLink({ className = "", href, text, white = false} : IProps) {
 	return (
-		<Link href={href} className={`${className} ${white && "text-white border-white"} inline-flex justify-center font-bold border-solid border-2 py-3 px-5 uppercase`}>
+		<Link href={href} className={`${className} ${white && "text-white border-white"} inline-flex justify-center font-bold border-solid border-2 py-3 px-8 uppercase shadow-md active:translate-y-[2px]`}>
 			{text}
 		</Link>
 	)
