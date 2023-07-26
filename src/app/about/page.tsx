c468dda6e77c3ce27d955207d4f23915fe4a3376
Alexander <alexander.work.mailbox@gmail.com>
2023-07-26 14:17:25 +0600

    Corrected about page styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ async function About() {
 
 	return (
 		<div className="container">
-			<section className="my-5 card p-3 text-center md:text-left">
+			<section className="my-5 card p-3 md:min-h-[324px] text-center md:text-left">
 				<div className="w-[220px] xm:w-[300px] h-[200px] xm:h-[300px] mx-auto mb-3 md:float-left md:mr-5 border-solid border border-gray-400 shadow">
 					<Image src="/images/about/picture.jpg" width={300} height={400} alt="robot" className="w-full h-full object-cover object-center" />
 				</div>
