40b82a9e96f735ead49612c2dc472d3ab0ef1b68
Alexander <alexander.work.mailbox@gmail.com>
2023-07-28 15:04:49 +0600

    Added contacts form

---
+++ src/app/(Admin)/admin/contacts/page.tsx
@@ -0,0 +1,188 @@
+"use client"
+
+import Loader from "@/components/Loader";
+import useCustomSWR from "@/hooks/useCustomSWR";
+import axios from "axios";
+import { useFormik } from "formik";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+function Contacts() {
+
+	const [ errorMessage, setErrorMessage ] = useState("");
+	const [ success, setSuccess ] = useState(false);
+
+	const { data, isLoading } = useCustomSWR("/api/contacts");
+
+	const formik = useFormik({
+		initialValues: {
+			phone: "",
+			email: "",
+			linkedin: "",
+			github:  "",
+			skype: "",
+			messengers: ""
+		},
+		async onSubmit(values) {
+			if(!haveValuesChanged(values)) return;
+
+			try {
+				axios.put("/api/contacts", values);
+				setSuccess(true);
+			}
+			catch(e: any) {
+				console.log(e);
+				setErrorMessage(e.response.body || e.message || "Unknown error");
+			}
+		}
+	})
+
+	useEffect(() => {
+		if(data) formik.setValues(data);
+	}, [data])
+
+	function haveValuesChanged(values: any) {
+		for(let key in values) {
+			if(data[key] !== values[key]) return true;
+		}
+
+		return false;
+	}
+
+	if( isLoading ) return <Loader />
+
+	return (
+		<div className="container">
+			<div className="py-4">
+				<h1 className="title">
+					Contacts
+				</h1>
+				{
+					success &&
+					<div className="success mb-5 p-2 text-center font-bold">
+						Success
+					</div>
+				}
+				{
+					errorMessage &&
+					<div className="error mb-5 p-2 text-center font-bold">
+						{errorMessage}
+					</div>
+				}
+				<form onSubmit={formik.handleSubmit}>
+					<div className="flex mb-5">
+						<ul className="w-1/2">
+							<li className="relative mb-4 pl-8">
+								<div className="absolute left-0 top-0 w-[24px]">
+									<Image src="/images/icons/phone.png" width={24} height={24} alt="phone icon" />
+								</div>
+								<label htmlFor="phone" className="block mb-2">
+									Phone
+								</label>
+								<input
+									type="tel"
+									name="phone"
+									id="phone"
+									value={formik.values.phone}
+									onChange={formik.handleChange}
+									className="min-w-[300px] border border-solid border-gray-300 px-3 py-1"
+								/>
+							</li>
+							<li className="relative mb-4 pl-8">
+								<div className="absolute left-0 top-0 w-[24px]">
+									<Image src="/images/icons/linkedin.png" width={24} height={24} alt="phone icon" />
+								</div>
+								<label htmlFor="linkedin" className="block mb-2">
+									LinkedIn
+								</label>
+								<input
+									type="text"
+									name="linkedin"
+									id="linkedin"
+									value={formik.values.linkedin}
+									onChange={formik.handleChange}
+									className="min-w-[300px] border border-solid border-gray-300 px-3 py-1"
+								/>
+							</li>
+							<li className="relative mb-4 pl-8">
+								<div className="absolute left-0 top-0 w-[24px]">
+									<Image src="/images/icons/skype.png" width={24} height={24} alt="phone icon" />
+								</div>
+								<label htmlFor="skype" className="block mb-2">
+									Skype
+								</label>
+								<input
+									type="text"
+									name="skype"
+									id="skype"
+									value={formik.values.skype}
+									onChange={formik.handleChange}
+									className="min-w-[300px] border border-solid border-gray-300 px-3 py-1"
+								/>
+							</li>
+						</ul>
+						<ul className="w-1/2">
+							<li className="relative mb-4 pl-8">
+								<div className="absolute left-0 top-0 w-[24px]">
+									<Image src="/images/icons/email.png" width={24} height={24} alt="phone icon" />
+								</div>
+								<label htmlFor="email" className="block mb-2">
+									Email
+								</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									value={formik.values.email}
+									onChange={formik.handleChange}
+									className="min-w-[300px] border border-solid border-gray-300 px-3 py-1"
+								/>
+							</li>
+							<li className="relative mb-4 pl-8">
+								<div className="absolute left-0 top-0 w-[24px]">
+									<Image src="/images/icons/github.png" width={24} height={24} alt="phone icon" />
+								</div>
+								<label htmlFor="github" className="block mb-2">
+									Github
+								</label>
+								<input
+									type="text"
+									name="github"
+									id="github"
+									value={formik.values.github}
+									onChange={formik.handleChange}
+									className="min-w-[300px] border border-solid border-gray-300 px-3 py-1"
+								/>
+							</li>
+							<li className="relative mb-4 pl-8">
+								<div className="absolute left-0 top-0 w-[24px]">
+									<Image src="/images/icons/telegram.png" width={24} height={24} alt="phone icon" />
+								</div>
+								<label htmlFor="messengers" className="block mb-2">
+									Messengers
+								</label>
+								<input
+									type="text"
+									name="messengers"
+									id="messengers"
+									value={formik.values.messengers}
+									onChange={formik.handleChange}
+									className="min-w-[300px] border border-solid border-gray-300 px-3 py-1"
+								/>
+							</li>
+						</ul>
+					</div>
+					<div className="">
+						<input
+							type="submit"
+							value="Confirm"
+							className="btn py-2 px-3"
+						/>
+					</div>
+				</form>
+			</div>
+		</div>
+	)
+}
+
+export default Contacts;
---
+++ src/app/api/contacts/route.ts
@@ -0,0 +1,23 @@
+import db from "@/model/db.js";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+	const {id, ...contacts} = (await db.query("SELECT * FROM contacts;")).rows[0];
+
+	return NextResponse.json(contacts);
+}
+
+export async function PUT(req: NextRequest) {
+	const contacts = await req.json();
+
+	try {
+		await db.query("UPDATE contacts SET phone = $1, email = $2, linkedin = $3, github = $4, skype = $5, messengers = $6;", [contacts.phone, contacts.email, contacts.linkedin, contacts.github, contacts.skype, contacts.messengers]);
+	} catch (e: any) {
+		console.log(e);
+		return NextResponse.json(e, {
+			status: 500
+		})
+	}
+
+	return NextResponse.json("OK");
+}
