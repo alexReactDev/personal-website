e4903b708d083bc6bdd8e138f754072b7dd91fd4
Alexander <alexander.work.mailbox@gmail.com>
2023-08-31 18:27:43 +0600

    Moved database port to .env

--- src/model/db.js
+++ src/model/db.js
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import pg from "pg";
 
 const pool = new pg.Pool({
 	user: "postgres",
 	password: process.env.DB_PASSWORD,
 	host: "localhost",
-	port: 5432,
+	port: process.env.DB_PORT,
 	database: "personal_website"
 });
 
