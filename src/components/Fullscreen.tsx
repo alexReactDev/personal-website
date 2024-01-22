2670bf4e0f71bb758f38ee37dc699808e72400ae
Alexander <alexander.work.mailbox@gmail.com>
2024-01-22 17:08:38 +0400

    Fixed problem with fullscreen not full on responsive

--- src/components/Fullscreen.tsx
+++ src/components/Fullscreen.tsx
@@ -2,7 +2,7 @@ import ScrollDown from "./ScrollDown";
 
 function Fullscreen() {
 	return (
-		<section className="flex fullscreen -mt-[128px] pt-[128px] bg-[url('/images/homepage/main.jpg')] bg-center bg-cover bg-no-repeat text-white">
+		<section className="flex fullscreen -mt-[80px] pt-[80px] 2xm:-mt[100px] 2xm:pt-[100px] lg:-mt-[128px] lg:pt-[128px] bg-[url('/images/homepage/main.jpg')] bg-center bg-cover bg-no-repeat text-white">
 			<div className="flex flex-col container">
 				<div className="grow flex flex-col justify-center items-center text-center font-medium">
 					<h1 className="text-4xl xm:text-6xl sm:text-8xl mb-8" data-testid="main-title">
